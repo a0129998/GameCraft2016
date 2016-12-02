d83e14bb68264acdc604020b44c2223fb5c77a26
Tuong Van <Tuong Van>
2016-12-03 06:56:46 +0800

    Check for bounds

--- Assets/scripts/boardManager.cs
+++ Assets/scripts/boardManager.cs
@@ -29,13 +29,21 @@ public class boardManager : MonoBehaviour {
 		}
 	}
 
+	int getRows(){
+		return rows;
+	}
+
+	int getCols(){
+		return cols;
+	}
+
 	void BoardSetup(){
 		boardHolder = new GameObject ("Board").transform;
 
 		for (int x = 0; x < cols; x++) {
 			for (int y = 0; y < rows; y++) {
 				GameObject toInstantiate = backgroundTiles [Random.Range (0, backgroundTiles.Length)];
-				GameObject instance = Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
+				GameObject instance = Instantiate (toInstantiate, new Vector3 (x-cols/2, y-rows/2, 0f), Quaternion.identity) as GameObject;
 				instance.transform.SetParent (boardHolder);
 				Vector3 originalScale = instance.transform.localScale;
 				instance.transform.localScale = originalScale / 3 - new Vector3(0.1f, 0.1f, 0f);
--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -5,16 +5,56 @@ public class playerController : MonoBehaviour {
 
 	private int x;
 	private int y;
+	private int move=-1;
+	public boardManager bm;
+	private int lower_x, lower_y, upper_x, upper_y;//the bounds for legal moves
+	private int[,] moves = new int[2,4] { {0, 0, -1, 1}, {-1, 1, 0, 0}};//to determine the coords of the next move
+
+	void Awake(){
+		lower_x = -2;//TO CHANGE TO REFLECT THE ROWS AND COLS FROM boardManager
+		upper_x = 2;
+		lower_y = -2;
+		upper_y = 2;
+	}
+
+	bool inRange(int nx, int ny){
+		return nx <= upper_x && nx >= lower_x && ny <= upper_y && ny >= lower_y;
+	}
+
+	void makeMove(int moveIndx){
+		x += moves[0, moveIndx];
+		y += moves[1, moveIndx];
+		switch (moveIndx) {
+		case 0:
+			this.transform.position += Vector3.up;
+			break;
+		case 1:
+			this.transform.position += Vector3.down;
+			break;
+		case 2:
+			this.transform.position += Vector3.left;
+			break;
+		case 3:
+			this.transform.position += Vector3.right;
+			break;
+		}
+	}
 
 	void Update(){
+		move = -1;
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			this.transform.position += Vector3.up;
+			move = 0;
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			this.transform.position += Vector3.down;
+			move = 1;
 		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			this.transform.position += Vector3.left;
+			move = 2;
 		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			this.transform.position += Vector3.right;
+			move = 3;
+		}
+
+		if(move > -1 && inRange(x+moves[0, move], y+moves[1, move])){
+			makeMove (move);
+			Debug.Log ("x = " + x + " y= " + y);
 		}
 	}
 }
